099b0154668879178021178a2fd41e7420f26e85
Matthew2025-byte <mdstapleton2020@outlook.com>
2025-05-01 14:12:14 -0700

    added geometric_series

--- sequence/geometric.py
+++ sequence/geometric.py
@@ -7,8 +7,28 @@ class geometric_sequence:
     
     def __getitem__(self, index):
         return self.start * self.ratio ** (index - 1)
-    
+
+
 
 class geometric_series:
     def __init__(self) -> None:
         pass
+
+    @staticmethod
+    def find_finite(**kwargs): # Too Complicated
+        """use a_1, r, and n
+        \nor use an a_n=[], starting with a_1
+        """
+        if 'a_1' in kwargs and 'r' in kwargs and 'n' in kwargs:
+            return (kwargs['a_1'] * (1 - kwargs['r'] ** kwargs['n'])) / (1 - kwargs['r'])
+        else:
+            ratio = kwargs['a_n'][1] / kwargs['a_n'][0]
+            return (kwargs['a_n'][0] * (1 - ratio**len(kwargs['a_n']))) / (1 - ratio)
+        
+    @staticmethod
+    def find_infinite(series: list[float]):
+        ratio = series[1] / series[0]
+        if ratio > 1:
+            return None
+        return series[0] / (1 - ratio)
+
