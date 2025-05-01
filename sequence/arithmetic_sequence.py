75d5793e97d242796c5046647fc5775021cb9a95
Matthew2025-byte <mdstapleton2020@outlook.com>
2025-05-01 12:13:52 -0700

    changed from interval to difference

--- sequence/arithmetic_sequence.py
+++ sequence/arithmetic_sequence.py
@@ -1,9 +1,9 @@
 class arithmetic_sequence:
     # Sum of an Arithmetic Series:
     # S_n = n/2 * (a_1 + a_n)
-    def __init__(self, interval, index, value):
-        self.interval = interval
-        self.start = (index - 1) * self.interval - value
+    def __init__(self, difference, index, value):
+        self.difference = difference
+        self.start = (index - 1) * self.difference - value
 
     def __getitem__(self, index):
-        return self.start + (index - 1) * self.interval
+        return self.start + (index - 1) * self.difference
