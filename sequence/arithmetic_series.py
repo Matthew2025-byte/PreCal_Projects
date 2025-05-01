c0ee15f8c5ef589f26f11b44448c3a45350fd0fb
Matthew2025-byte <mdstapleton2020@outlook.com>
2025-05-01 11:38:17 -0700

    Added Aritmetic Series logic

--- sequence/arithmetic_series.py
+++ sequence/arithmetic_series.py
@@ -1,4 +1,9 @@
 class arithmetic_series:
     # Sum of an Arithmetic Series:
     # S_n = n/2 * (a_1 + a_n)
-    ...
+    def __init__(self, interval, index, value):
+        self.interval = interval
+        self.start = (index - 1) * self.interval - value
+
+    def __getitem__(self, index):
+        return self.start + (index - 1) * self.interval
