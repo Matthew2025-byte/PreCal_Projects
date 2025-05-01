10326c624afffbb6d964a4628eb4bb9b795263ee
Matthew2025-byte <mdstapleton2020@outlook.com>
2025-05-01 12:16:04 -0700

    name changes

--- sequence/arithmetic_sequence.py
+++ sequence/arithmetic_sequence.py
@@ -1,6 +1,4 @@
 class arithmetic_sequence:
-    # Sum of an Arithmetic Series:
-    # S_n = n/2 * (a_1 + a_n)
     def __init__(self, difference, index, value):
         self.difference = difference
         self.start = (index - 1) * self.difference - value
--- sequence/sequence.py
+++ sequence/sequence.py
@@ -1,7 +1,7 @@
 class arithmetic_sequence:
-    def __init__(self, equation: str, **kwargs):
+    def __init__(self, formula: str, **kwargs):
         self.kwargs = kwargs
-        self.equation = equation
+        self.formula = formula
         return
     
     def __getitem__(self, index):
@@ -12,19 +12,19 @@ class arithmetic_sequence:
     
     def unsimplified_equation(self, index):
         self.kwargs["n"] = index
-        equation = self.equation
+        formula = self.formula
         for key, value in self.kwargs.items():
             try:
-                equation = equation.replace(key, str(value))
+                formula = formula.replace(key, str(value))
             except:
                 continue
-        return equation
+        return formula
     
 
 
 
 
-def sigma(start, end, equation) -> list:
-    seq = arithmetic_sequence(equation)
-    equations = [seq.unsimplified_equation(i) for i in range(start, end + 1)]
-    return eval("+".join(equations))
+def sigma(start, end, formula) -> list:
+    seq = arithmetic_sequence(formula)
+    formulas = [seq.unsimplified_equation(i) for i in range(start, end + 1)]
+    return eval("+".join(formulas))
